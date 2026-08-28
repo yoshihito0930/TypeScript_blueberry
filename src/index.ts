eaca31df86dfcaf7e82ed7ebbe40a54de594a122
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2026-08-29 05:56:27 +0900

    p.36_1 2進数、8進数、16進数

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-const width1 = 5;
-const width2 = 8;
-const height: number = 3;
-const area = (width1 + width2) * height / 2;
+const binary = 0b1010;
+const octal = 0o755;
+const hexadecimal = 0xff;
 
-console.log(area);
+console.log(binary, octal, hexadecimal);
