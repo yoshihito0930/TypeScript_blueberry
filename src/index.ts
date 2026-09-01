04eff1eebd38884b8109622cd033cd6862080900
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2026-09-02 01:37:18 +0900

    p.42_1 readlineを使うには、tsconfigのtypesにnodeを指定する必要があった。 src/index.ts:1:33 - error TS2591: Cannot find name 'readline'. Do you need to install type definitions for node? Try changed 1 package, and audited 5 packages in 2s
    found 0 vulnerabilities and then add 'node' to the types field in your tsconfig.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-const n: null = null;
-const u: undefined = undefined;
+import { createInterface } from 'readline';
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('文字列を入力してください:', (line) => {
+    console.log(`${line} が入力されました`);
+    rl.close();   
+});
