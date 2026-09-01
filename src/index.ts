2b201e5339005661d69124bbe4bca14751c0614b
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2026-09-02 01:57:37 +0900

    p.43_1 resultはstring型

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const rl = createInterface({
 });
 
 rl.question('数値を入力してください:', (line) => {
-    console.log(line + 1000);
+    const result = line + 1000;
+    console.log(result);
     rl.close();   
 });
