e278129616b7325dc406be5c4d4bd2a7262fe17c
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2026-09-03 01:02:04 +0900

    p.46_1 String型への変換

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-const bigint = BigInt("fooooooooooo");
-console.log("bigint is ", bigint);
+const str1 = String(1234.5);
+console.log(str1);
+
+const str2 = String(true);
+console.log(str2);
+
+const str3 = String(null);
+const str4 = String(undefined);
+console.log(str3, str4);
