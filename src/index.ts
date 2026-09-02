e7351140538572e4d85230f673320a23a1b584d5
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2026-09-03 00:45:53 +0900

    p.44_1 lineを数値型に変換

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const rl = createInterface({
 });
 
 rl.question('数値を入力してください:', (line) => {
-    const result = line + 1000;
-    console.log(result);
+    const num = Number(line);
+    console.log(num + 1000);
     rl.close();   
 });
