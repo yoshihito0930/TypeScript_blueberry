97457b42bb90fc103528057dbf76c39f2c5b2b7d
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2026-09-03 00:58:02 +0900

    p.45_3 BigIntにはNaNに相当する値がないため以下のエラーが発生する可能性がある。 const bigint = BigInt(fooooooooooo);                ^
    SyntaxError: Cannot convert fooooooooooo to a BigInt

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-const bigint1 = BigInt("1234");
-console.log(bigint1);
-
-const bigint2 = BigInt(500);
-console.log(bigint2);
-
-const bigint3 = BigInt(true);
-console.log(bigint3);
+const bigint = BigInt("fooooooooooo");
+console.log("bigint is ", bigint);
