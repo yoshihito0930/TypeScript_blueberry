0582bd6c3bbb231534b05b98e10fbe3cc46adc31
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2026-09-05 17:42:45 +0900

    p.54_2 論理演算子を使ったプログラム

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-const t = true, f = false;
+import { createInterface } from 'readline';
 
-console.log(t && t);
-console.log(t && f);
-console.log(f && f);
+const rl = createInterface ({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.log(t || t);
-console.log(t || f);
-console.log(f || f);
+rl.question('数値を入力してください:', (line) => {
+    const num = Number(line);
+    if (0 <= num && num < 100) {
+        console.log(`${num}は0以上100未満です`)
+    } else {
+        console.log(`${num}は0以上100未満ではありません`)
+    }
+    rl.close();
+});
