e9eb74f446e86d39eb59b195ef66bef63c9a903b
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2026-09-05 17:09:53 +0900

    p50_3 +は文字列の結合にも使える

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-let foo = 10;
-console.log(++foo);
-console.log(foo--);
+import { createInterface } from 'readline';
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('名前を入力してください:', (name) => {
+    console.log("こんにちは、" + name + "さん");
+    rl.close();
+})
