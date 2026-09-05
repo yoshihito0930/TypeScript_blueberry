8f54a13843e5b39f42ebda28cb7242d7f3e256a3
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2026-09-05 17:18:14 +0900

    p.51_2 大小比較の例

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-console.log("foo" + true);
-console.log(null + "bar");
+const left1 = -5, right1 = 0;
+console.log(left1 < right1);
+
+const left2 = 100n, right2 = 50n;
+console.log(left2 >= right2);
+
+const left3 = -10, right3 = 0;
+console.log(left3 > right3);
+
+const left4 = 12n, right4 = 8n;
+console.log(left4 <= right4);
