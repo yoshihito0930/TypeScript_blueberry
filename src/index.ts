a2fdfa2dda568b64efdea6beb0e49d2378550ded
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2026-09-05 17:14:05 +0900

    p51_1 左右どちらかが文字列であれば、どんな値でも渡せることができる。 テンプレートリテラル`を使うことで以下のように書くことができ、文字列結合に+を使う機会は減っている。 `こんにちは、${name}さん`

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-import { createInterface } from 'readline';
-
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('名前を入力してください:', (name) => {
-    console.log("こんにちは、" + name + "さん");
-    rl.close();
-})
+console.log("foo" + true);
+console.log(null + "bar");
